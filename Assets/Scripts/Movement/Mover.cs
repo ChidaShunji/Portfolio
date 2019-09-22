4d5c14f69c2db8448c8d51b56dccb12f5f9181cf
Chida Shun <cs0518xxx@gmail.com>
2019-09-22 13:05:10 +0900

    容量削減

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+           MoveToCursor();
+        }
+        //animationを付随させる
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hashit = Physics.Raycast(ray, out hit);
+        if (hashit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+    //animationを追加
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localvelocity = transform.InverseTransformDirection(velocity); 
+        float speed = localvelocity.z;
+        GetComponent<Animator>().SetFloat("forwardspeed", speed);
+    }
+
+
+}
+
+   
+    
